73c2637cfbc1ed9c4ca94d6cfdb2d6d114c7ea98
Olivia Yu <olivia.yu@liferay.com>
2025-11-17 14:45:35 -0800

    Import scss file directly

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {ClayIconSpriteContext} from "@clayui/icon";
 
-import "../../liferay-portal-master/portal/modules/apps/frontend-theme/frontend-theme-classic/build/css/main.css";
-import "../../liferay-portal-master/portal/modules/apps/frontend-theme/frontend-theme-classic/build/css/clay.css";
+import "../../liferay-portal-master/portal/modules/apps/frontend-theme/frontend-theme-classic/build/css/main.scss";
+import "../../liferay-portal-master/portal/modules/apps/frontend-theme/frontend-theme-classic/build/css/clay.scss";
 
 import "src/main/resources/META-INF/resources/css/main.scss";
 

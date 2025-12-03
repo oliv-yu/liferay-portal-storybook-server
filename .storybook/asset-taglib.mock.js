fc7cb7d7bf64012a21bb50ca9469d0371276900a
Olivia Yu <olivia.yu@liferay.com>
2025-12-02 16:14:23 -0800

    Move mock files + add frontend-js-item-selector-web.mock

--- .storybook/asset-taglib.mock.js
+++
@@ -1,7 +0,0 @@
-import AssetCategoriesSelector from "../static/mock-components/src/main/resources/META-INF/resources/asset_categories_selector/AssetCategoriesSelector";
-import AssetVocabularyCategoriesSelector from "../static/mock-components/src/main/resources/META-INF/resources/asset_categories_selector/AssetVocabularyCategoriesSelector";
-
-module.exports = {
-	AssetCategoriesSelector,
-	AssetVocabularyCategoriesSelector,
-};

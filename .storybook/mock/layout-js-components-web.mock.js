2060796419aae4878079a6711af2e7a2fa267878
Olivia Yu <olivia.yu@liferay.com>
2026-05-13 15:45:39 -0700

    Fix path (not official)

--- .storybook/mock/layout-js-components-web.mock.js
+++ .storybook/mock/layout-js-components-web.mock.js
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-export {RowBuilder} from "../../../portal/modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/components/row_builder/RowBuilder";
+export {RowBuilder} from "../../../liferay-portal/modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/components/row_builder/RowBuilder";
 export {
 	ScreenReaderAnnouncerContext,
 	ScreenReaderAnnouncerContextProvider,
-} from "../../../portal/modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/contexts/ScreenReaderContext";
+} from "../../../liferay-portal/modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/contexts/ScreenReaderContext";

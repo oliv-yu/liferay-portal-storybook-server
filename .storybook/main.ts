f7d67fe436c082140513da925cf18baacb1060b6
Olivia Yu <olivia.yu@liferay.com>
2026-06-12 10:56:14 -0700

    Use portalPath alias instead of hardcoding every portal path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -52,6 +52,7 @@ const config: StorybookConfig = {
 				...config.resolve,
 				alias: {
 					...(config?.resolve?.alias || {}),
+					"@portal": portalPath,
 					"atlas-variables": require.resolve(
 						path.join(
 							portalPath,
